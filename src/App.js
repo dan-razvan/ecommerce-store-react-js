a2ec3bff84c7ffaab8f1f96aac5046526944bd66
dan-razvan <razvanbogdanturiatca@gmail.com>
2022-03-10 22:49:20 +0200

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
 import Homepage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shoppage/shop.component'
 
 import './App.css'
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
-
-const JacketsPage = () => (
-  <div>
-    <h1>JACKETS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
-        <Route path="/jackets" component={JacketsPage} />
+        <Route exact path="/shop" component={ShopPage} />
+        {/* <Route path="/hats/:topicId" component={WhiteHats} /> */}
       </Switch>
     </div>
   )
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,17 @@
 import react from 'react'
 import './homepage.styles.scss'
 import Directory from '../../components/directory/directory.component'
+import { Link } from 'react-router-dom'
 
-const Homepage = () => (
-  <div className="homepage">
-    <Directory />
-  </div>
-)
+const Homepage = (props) => {
+  console.log(props)
+  return (
+    <div className="homepage">
+      {/* <Link to="/hats">Hats</Link> */}
+      <button onClick={() => props.history.push('/hats')}>Hats</button>
+      <Directory />
+    </div>
+  )
+}
 
 export default Homepage
